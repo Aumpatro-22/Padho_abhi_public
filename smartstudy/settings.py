79d7b2883c528199ad02610205ae6e66dccef1f7
Aum Patro <192595024+Aumpatro-22@users.noreply.github.com>
2026-01-09 00:22:59 +0530

    Fix database config: handle empty DATABASE_URL explicitly

--- smartstudy/settings.py
+++ smartstudy/settings.py
@@ -88,13 +88,20 @@ WSGI_APPLICATION = "smartstudy.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
